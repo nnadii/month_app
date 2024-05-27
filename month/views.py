0349c44635766eb61b43833a26c11224c51dc3a9
Chimennadi <nnadichime04@gmail.com>
2024-05-27 09:13:44 +0100

    dictionaries added

--- month/views.py
+++ month/views.py
@@ -9,20 +9,20 @@ from .models import Month
 # Create your views here.
 
 
-# app_settings_func = {
-#     "january": "Eat no meat for the entire month!",
-#     "febuary": "Walk for at least 20 minutes every day!",
-#     "march": "Learn Django for at least 20 minutes every day!",
-#     "april": "Eat no meat for the entire month!",
-#     "may": "Walk for at least 20 minutes every day!",
-#     "june": "Learn Django for at least 20 minutes every day!",
-#     "july": "Eat no meat for the entire month!",
-#     "august": "Walk for at least 20 minutes every day!",
-#     "september": "Learn Django for at least 20 minutes every day!",
-#     "october": "Eat no meat for the entire month!",
-#     "november": "Walk for at least 20 minutes every day!",
-#     "december": "Eat no meat for the entire month!"
-# }
+app_settings_func = {
+    "january": "Eat no meat for the entire month!",
+    "febuary": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": "Eat no meat for the entire month!"
+}
 
 # Create your views here.
 
